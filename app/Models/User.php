78d5649c117dd87e011df11c421f8f746269d4ee
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-03-16 17:57:35 +0100

    add fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'date_of_birth'
+
     ];
 
     /**
