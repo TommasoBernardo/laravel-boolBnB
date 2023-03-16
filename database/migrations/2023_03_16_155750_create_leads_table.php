4d1263a1441c05eaacb05167647beca606c90e70
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-16 17:04:55 +0100

    create leads table

---
+++ database/migrations/2023_03_16_155750_create_leads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('apartment_id');
+            $table->string('email', 255)->nullable(false);
+            $table->string('name', 150)->nullable(false);
+            $table->text('message')->nullable(false);
+            $table->tinyInteger('phone_number')->nullable()->unsigned();
+            $table->boolean('show')->default(true);
+            $table->timestamps();
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+};
