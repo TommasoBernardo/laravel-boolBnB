db58db14a4430476aad0b669110fce4795151c08
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-17 10:44:21 +0100

    create ApartmentServiceSeeder

---
+++ database/seeders/ApartmentServiceSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apartment;
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApartmentServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $apartments = Apartment::all();
+        foreach ($apartments as $apartment) {
+            $apartment->services()->attach(Service::inRandomOrder()->limit(7)->get());
+        }
+    }
+}
