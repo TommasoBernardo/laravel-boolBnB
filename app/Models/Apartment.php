dc7a2f3bcba1682df9ead034f43ff08becab7da0
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-17 10:43:38 +0100

    added relation method

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
 }
