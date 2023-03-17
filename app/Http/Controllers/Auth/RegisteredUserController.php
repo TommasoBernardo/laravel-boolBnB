cd0fc450300fc530031c253f7a0eb13cee929f83
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-17 12:36:55 +0100

    update validation rules for users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,14 +30,38 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        $request->validate(
+            [
+                'name' => ['string', 'min:3', 'nullable', 'max:255'],
+                'surname' => ['string', 'min:3', 'nullable', 'max:150'],
+                'date_of_birth' => ['date', 'nullable'],
+                'email' => ['required', 'string', 'email', 'min:8', 'max:255', 'unique:' . User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ],
+            [
+                'name.string' => 'Si accettano solo lettere',
+                'name.min' => 'Inserire minimo 3 caratteri ',
+                'name.max' => 'Inserire massimo 255 caratteri',
+                'surname.string' => 'Si accettano solo lettere',
+                'surname.min' => 'Inserire minimo 3 caratteri ',
+                'surname.max' => 'Inserire massimo 150 caratteri',
+                'date_of_birth.date' => 'Inserire una data valida',
+                'email.required' => 'Email obbligatorio',
+                'email.email' => 'Inserire un email valida',
+                'email.unique' => 'Email gia presente',
+                'email.min' => 'Inserire minimo 8 caratteri',
+                'email.max' => 'Inserire massimo 255 caratteri',
+                'password.required' => 'Password obbligatoria',
+                'password.confirmed' => 'Password diversa'
+
+            ]
+
+        );
 
         $user = User::create([
             'name' => $request->name,
+            'surname' =>  $request->surname,
+            'date_of_birth' => $request->date_of_birth,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
