43f27dde1dec6b81d7c81f8b3f43753c924c68e7
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-03-17 18:50:16 +0100

    add js scripts tomtom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,15 @@
 
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js"></script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js"></script>
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
+
+
+
 </head>
 
 <body>
@@ -89,6 +93,7 @@
             @yield('content')
         </main>
     </div>
+    @yield('script')
 </body>
 
 </html>
