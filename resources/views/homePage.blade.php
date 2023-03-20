75d8db4cf21560d2b9a05210f1268deb15d3b294
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-20 18:29:43 +0100

    add apartmanets, and layouts

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -9,13 +9,13 @@
 
                 <div class="row justify-content-center align-items-center">
                     <div class="col-lg-9 text-center">
-                        <h1 class="heading"  >
+                        <h1 class="heading">
                             Easiest way to find your dream home
                         </h1>
                         <form action="#" class="narrow-w form-search d-flex align-items-stretch mb-3 mt-5"
                             data-aos="fade-up" data-aos-delay="200">
                             <input type="text" class="form-control px-4" placeholder=" New York" />
-                            <button type="submit" class="btn btn-outline-success ms-2 ">Search</button>
+                            <button type="submit" class="btn btn-outline-success ms-2">Search</button>
                         </form>
                     </div>
                 </div>
@@ -35,39 +35,39 @@
                 </p>
             </div>
         </div>
-        <div class="property-item">
-            <a href="property-single.html" class="img">
-              <img src="" alt="Image" class="img-fluid" />
-            </a>
 
-            <div class="property-content">
-              <div class="price mb-2">
-                <p>
+        <div class="row">
+                @foreach ($apartments as $apartment)
+                <div class="col-3">
+                    <div class="card-custom">
+                        <div href="property-single.html" class="img">
+                            <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top"  style="width:250px; height:250px ; border-radius:2rem; " alt="{{ $apartment->title }}">
+                        </div>
+                        <div class="property-content">
+                            <div class="price mb-1">
+                                <p>
+                                </p>
+                            </div>
+                            <div class="mb-3">
+                                <span class="d-block mb-2 text-black-50">{{ $apartment->title}}</span>
+                                <span class="city d-block mb-1">{{ $apartment->address}}</span>
+                                <div class="specs d-flex mb-2">
+                                    <span class="d-block d-flex align-items-center me-3">
+                                        <span class="caption">Stanze: {{ $apartment->rooms }}</span>
+                                    </span>
+                                    <span class="d-block d-flex align-items-center">
+                                        <span class="caption">Numeri letti {{ $apartment->beds }}</span>
+                                    </span>
+                                </div>
+                                <div class="" >
+                                    <a href="property-single.html" class="btn btn-outline-success rounded-5 ">See details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                </p>
-            </div>
-              <div>
-                <span class="d-block mb-2 text-black-50">5232 California Fake, Ave. 21BC</span>
-                <span class="city d-block mb-3">California, USA</span>
-
-                <div class="specs d-flex mb-4">
-                  <span class="d-block d-flex align-items-center me-3">
-                    <span class="icon-bed me-2"></span>
-                    <span class="caption">2 beds</span>
-                  </span>
-                  <span class="d-block d-flex align-items-center">
-                    <span class="icon-bath me-2"></span>
-                    <span class="caption">2 baths</span>
-                  </span>
                 </div>
-
-                <a
-                  href="property-single.html"
-                  class="btn btn-primary py-2 px-3"
-                  >See details</a
-                >
-              </div>
-            </div>
-          </div>
+                @endforeach
+        </div>
     </div>
-    @endsection
+@endsection
