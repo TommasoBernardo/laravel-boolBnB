16bf1e327cf2797877120b34219c66b0d255311b
TommasoBernardo <tommasob1210@gmail.com>
2023-03-21 18:38:53 +0100

    create show

---
+++ resources/js/show.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import '~resources/scss/app.scss';
+import '~resources/scss/front-show-apartments/show-apartment.scss';
+import * as bootstrap from 'bootstrap';
+import.meta.glob([
+    '../img/**'
+])
