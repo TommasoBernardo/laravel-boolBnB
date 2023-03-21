f59970b936f9deb9353a27eeb001d091b14b9b0c
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-21 18:04:37 +0100

    created show-ur

--- resources/views/ur/apartment/show.blade.php
+++ resources/views/ur/apartment/show.blade.php
@@ -1,22 +1,71 @@
 @extends('layouts.app')
-
+@section('scss')
+    @vite(['resources/js/show.js'])
+@endsection
 @section('content')
-    <div class="container">
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top" alt="{{ $apartment->title }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $apartment->title }}</h5>
-                <p class="card-text">Si trova in: {{ $apartment->address }}</p>
-                <p class="card-text">Numero di stanze: {{ $apartment->rooms }}</p>
-                <p class="card-text">Numero di letti: {{ $apartment->beds }}</p>
-                <p class="card-text">Numero di bagni: {{ $apartment->bathrooms }}</p>
-                <p class="card-text">Servizi disponibili:
+    <div class="container img-container p-5 my-4 ">
+        <h1 class="m-4 text-md-center">{{ $apartment->title }}</h1>
+        <div class="row p-5">
+            <div class="col-lg-6 col-md-12">
+                <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                    alt="{{ $apartment->title }}">
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top "
+                            alt="{{ $apartment->title }}">
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                            alt="{{ $apartment->title }}">
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                            alt="{{ $apartment->title }}">
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                            alt="{{ $apartment->title }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class=" container info p-3">
+            <div class="row px-2">
+                <h3 class="my-2">Info appartamento</h3>
+                <div class="col-lg-6  col-md-6 col-sm-12">
+                    <p>Numero di stanze: {{ $apartment->rooms }}</p>
+                    <p>Numero di letti: {{ $apartment->beds }}</p>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                    <p>Numero di bagni: {{ $apartment->bathrooms }}</p>
+                    <p>Dimensioni: {{ $apartment->square_meters }} mq</p>
+                </div>
+            </div>
+        </div>
+        <div class="services p-3 my-3">
+            <div class="container">
+                <div class="row px-2">
+                    <h3 class="my-2">Servizi offerti</h3>
                     @foreach ($apartment->services as $service)
-                        {{ $service->type }}
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <span class="icon m-1 fs-4">
+                                {!! $service->icon !!} &nbsp {{ $service->type }}
+                            </span>
+                        </div>
                     @endforeach
-                </p>
-                <p class="card-text">Dimensioni: {{ $apartment->square_meters }}</p>
-                <a href="#" class="btn btn-primary">Entra</a>
+                </div>
+            </div>
+        </div>
+        <div class="container location">
+            <h3 class="d-inline-block my-3">Si trova in </h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <span class="fs-5">Via {{ $apartment->address }}</span>
+                    <div class="map my-2">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
