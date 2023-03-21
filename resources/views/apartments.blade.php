926bec955579e166caccf654825481f349dfda77
TommasoBernardo <tommasob1210@gmail.com>
2023-03-21 12:27:30 +0100

    update file for aestetichs

--- resources/views/apartments.blade.php
+++ resources/views/apartments.blade.php
@@ -4,24 +4,26 @@
 @endsection
 @section('content')
     <div class="container">
-        <div class="mt-3">
-            <h1>
-                All Apartments :
+        <div class="mt-3 text-center">
+            <h1 class="fw-bold">
+                All Apartments
             </h1>
         </div>
-        <div class="row">
+        <div class="row ">
             @foreach ($apartments as $apartment)
-                <div class="col-4">
-                    <div class="card mt-5" style="width: 22rem; height: 30rem;">
-                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-3 justify-content-sm-center ">
+                    <div id="my-card" class="card mt-5" style=" height: 34rem;">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid "
                             style="height: 18rem;" alt="{{ $apartment->title }}">
                         <div class="card-body">
-                            <p class="card-text">si trova a : {{ $apartment->address }}</p>
-                            <p class="card-text">Numero di stanze:{{ $apartment->rooms }}
+                            <p class="card-text"> {{ $apartment->address }}</p>
+                            <p class="card-text my-title fw-bold"> {{ $apartment->title }}</p>
+
+                            <p class="card-text">{{ $apartment->rooms }} : stanze
                             </p>
-                            <p class="card-text">Numero di letti: {{ $apartment->beds }}</p>
+                            <p class="card-text"> {{ $apartment->beds }} : letti</p>
                             <a href="{{ route('apartment.show', $apartment->slug) }}"
-                                class="btn btn-success   btn-green">Show</a>
+                                class="btn placeholder-glow  btn-green" id="border">Show more </a>
 
 
                             </p>
