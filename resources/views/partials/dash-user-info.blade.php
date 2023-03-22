82950a7585f36b24586134259e161bcfdbf0f766
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-22 17:46:43 +0100

    Text translation

--- resources/views/partials/dash-user-info.blade.php
+++ resources/views/partials/dash-user-info.blade.php
@@ -4,7 +4,7 @@
       <div class="d-flex flex-column align-items-center text-center">
         <img src="https://i.pravatar.cc/300" alt="Profile picture" class="rounded-circle" width="150">
         <div class="mt-3">
-          <h4>Ciao, {{Auth::user()->name }} {{Auth::user()->surname }} @if(!Auth::user()->name and !Auth::user()->surname) User {{Auth::user()->id}} @endif</h4>
+          <h4>Hi, {{Auth::user()->name }} {{Auth::user()->surname }} @if(!Auth::user()->name and !Auth::user()->surname) User {{Auth::user()->id}} @endif</h4>
           <p class="text-secondary mb-1">{{Auth::user()->email }}</p>
           <p class="text-muted font-size-sm">{{Auth::user()->date_of_birth }}</p>
         </div>
@@ -18,7 +18,7 @@
   <div class="col-lg-4 col-md-6 mb-4 d-flex align-items-stretch">
     <div class="card shadow-sm" style="width: 18rem;">
       <div class="card-title text-center ">
-        <h5 class="p-4">Appartamenti registrati</h5>
+        <h5 class="p-4">Registered apartments</h5>
         <p class="display-3 text-primary"><span class="count" total="{{count($apartments)}}">0</span></p>
       </div>
     </div>
@@ -26,7 +26,7 @@
   <div class="col-lg-4 col-md-6 mb-4 d-flex align-items-stretch">
     <div class="card shadow-sm" style="width: 18rem;">
       <div class="card-title text-center ">
-        <h5 class="p-4">Totale visualizzazioni</h5>
+        <h5 class="p-4">Total views</h5>
         <p class="display-3 text-primary"><span class="count" total="50">0</span></p>
       </div>
     </div>
@@ -34,7 +34,7 @@
   <div class="col-lg-4 col-md-12 mb-4 d-flex justify-content-center align-items-stretch">
     <div class="card shadow-sm" style="width: 18rem;">
       <div class="card-title text-center ">
-        <h5 class="p-4">Cosa a caso</h5>
+        <h5 class="p-4">Something</h5>
         <p class="display-3 text-primary"><span class="count" total="754">0</span></p>
       </div>
     </div>
