89798538c364fc97ca9fbc9c2d244b37a88b05ed
TommasoBernardo <tommasob1210@gmail.com>
2023-03-22 12:36:09 +0100

    mb-3

--- resources/views/apartmentShow.blade.php
+++ resources/views/apartmentShow.blade.php
@@ -64,7 +64,7 @@
                 Your position:
             </h3>
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 mb-3">
                     <div class="my-maps"></div>
                 </div>
             </div>
