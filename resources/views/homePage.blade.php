dae9f89837105d403eeb97ce3b3ad177adc40aa0
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-23 12:42:12 +0100

    fix media guery for title and search-box in jumbotron

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('scss')
-@vite(['resources/js/home.js'])
+    @vite(['resources/js/home.js'])
 @endsection
 @section('content')
+    {{-- to do: icone card/ titolo search-bar jumbo / bottone 'proprieties'  --}}
+
     {{-- Slider-carousel --}}
     <div class="container-carousel mb-5">
         <div id="carouselExampleIndicators" class="carousel slide hero-parent" data-bs-ride="carousel">
@@ -13,30 +15,27 @@
             </ol>
             <div class="search-box-hero d-flex justify-content-center align-items-center ">
                 <div class="row">
-                    <div class="col text-light" >
-                        <h1 class="heading fw-bold " >
+                    <div class="col  text-light">
+                        <h1 class="heading fw-bold ">
                             Easiest way to find your dream home <i class="fa-regular fa-paper-plane"></i>
                         </h1>
-                       <form action="#" class="narrow-w form-search d-flex align-items-stretch mb-3 ps-3  mt-5"
+                        <form action="#" class="narrow-w form-search d-flex align-items-stretch mb-3 ps-3  mt-5"
                             data-aos="fade-up" data-aos-delay="200">
                             <input type="text" class="form-control px-4" placeholder="es. New York" />
                             <button type="submit" class="btn btn-success ms-2">Search</button>
-                        </form> 
+                        </form>
                     </div>
                 </div>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active" style="height: 550px;" >
-                    <img src="{{ asset('storage/img/cover_img/cover_image-1.jpg') }}" class=" jumbo-img "
-                        alt="...">
+                <div class="carousel-item active" style="height: 550px;">
+                    <img src="{{ asset('storage/img/cover_img/cover_image-1.jpg') }}" class=" jumbo-img " alt="...">
                 </div>
-                <div class="carousel-item" style="height: 550px;" >
-                    <img src="{{ asset('storage/img/cover_img/cover_image-2.jpg') }}" class="jumbo-img"
-                        alt="...">
+                <div class="carousel-item" style="height: 550px;">
+                    <img src="{{ asset('storage/img/cover_img/cover_image-2.jpg') }}" class="jumbo-img" alt="...">
                 </div>
-                <div class="carousel-item" style="height: 550px;" >
-                    <img src="{{ asset('storage/img/cover_img/cover_image-3.jpg') }}" class="jumbo-img"
-                         alt="...">
+                <div class="carousel-item" style="height: 550px;">
+                    <img src="{{ asset('storage/img/cover_img/cover_image-3.jpg') }}" class="jumbo-img" alt="...">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
@@ -53,14 +52,15 @@
     <div class="container mb-5">
         <div class="row mb-5 align-items-center">
             <div class="col-lg-9 col-sm-3 ">
-                <h2 class="font-weight-bold heading fw-bold" style="color:rgb(0, 85, 85) " >
+                <h2 class="font-weight-bold heading fw-bold" style="color:rgb(0, 85, 85) ">
                     Popular Properties
                 </h2>
             </div>
-            <div class="col-lg-3 col-sm-3 d-flex  justify-content-end">
-                <div class="">
-                    <a href="{{route('apartments.index') }}" class="btn text-white custom-button rounded-5" style="background-color:rgb(0, 85, 85)">View all properties</a>
-                </div>
+            <div class="col-lg-3 col-sm-3 div-wrap-button-properties">
+
+                <a href="{{ route('apartments.index') }}" class="btn text-white custom-button rounded-5"
+                    style="background-color:rgb(0, 85, 85)">View all properties</a>
+
             </div>
         </div>
         <div class="container-fluid">
@@ -77,23 +77,27 @@
                                     </div>
                                     <div class="flex-column mt-3">
                                         <div class="mb-3">
-                                            <div class="mt-2" style="height: 70px " >
+                                            <div class="mt-2" style="height: 70px ">
                                                 <h6 class="d-block mb-2 fw-bold">{{ $apartment->title }}</h6>
                                                 <p class="city d-block fw-light mb-1"> {{ $apartment->address }}</p>
                                             </div>
-                                            <div class="d-flex mt-2" >
-                                                <p class="me-3" style="" ><i class="fa-solid fa-house"></i>   {{ $apartment->rooms }}</span>
-                                                <p class=""><i class="fa-solid fa-bed"></i> {{ $apartment->beds }} </span>
+                                            <div class="d-flex mt-2">
+                                                <p class="me-3" style=""><i class="fa-solid fa-house"></i>
+                                                    {{ $apartment->rooms }}</span>
+                                                <p class=""><i class="fa-solid fa-bed"></i> {{ $apartment->beds }}
+                                                    </span>
                                             </div>
-                                            <div class="mt-2" >
-                                                <a href="{{route('apartments.show', $apartment->slug ) }}"class="btn btn-outline-success rounded-5 "> See details </a>
+                                            <div class="mt-2">
+                                                <a
+                                                    href="{{ route('apartments.show', $apartment->slug) }}"class="btn btn-outline-success rounded-5 ">
+                                                    See details </a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         @endforeach
-                        
+
                     </div>
                     <div class="swiper-button-next"></div>
                     <div class="swiper-button-prev"></div>
@@ -106,59 +110,67 @@
     <div class="bg-color p-4">
         <div class="container mt-5 mb-5">
             <div class="row">
-               <div class="col-12">
+                <div class="col-12">
                     <h1 class="mb-3 fw-bold text-white text-center">
-                       Why people choose us
+                        Why people choose us
                     </h1>
-                </div> 
-               <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
-                   <div class="card border-0  " style="width: 18rem;">
-                       <div class="card-body text-center">
-                        <div class="icon mb-4">
-                            <i class="fa-solid fa-user"></i>
+                </div>
+                <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
+                    <div class="card border-0  " style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <div class="icon mb-4">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <h5 class="card-subtitle mb-2">Quality 1</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste
+                                quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias
+                                corporis voluptate quasi? Rerum!</p>
+                            <a href="#" class="link-about-us ">About Us</a>
                         </div>
-                         <h5 class="card-subtitle mb-2">Quality 1</h5>
-                         <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias corporis voluptate quasi? Rerum!</p>
-                         <a href="#" class="link-about-us ">About Us</a>
-                       </div>
                     </div>
-               </div>
-               <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
-                <div class="card border-0  " style="width: 18rem;">
-                    <div class="card-body text-center">
-                     <div class="icon mb-4">
-                        <i class="fa-solid fa-user"></i>
-                     </div>
-                      <h5 class="card-subtitle mb-2">Quality 2</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias corporis voluptate quasi? Rerum!</p>
-                      <a href="#" class="link-about-us ">About Us</a>
+                </div>
+                <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
+                    <div class="card border-0  " style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <div class="icon mb-4">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <h5 class="card-subtitle mb-2">Quality 2</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste
+                                quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias
+                                corporis voluptate quasi? Rerum!</p>
+                            <a href="#" class="link-about-us ">About Us</a>
+                        </div>
                     </div>
-                  </div>
-               </div>
-               <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
-                <div class="card border-0  " style="width: 18rem;">
-                    <div class="card-body text-center">
-                     <div class="icon mb-4">
-                        <i class="fa-solid fa-user"></i></i>
-                     </div>
-                      <h5 class="card-subtitle mb-2">Quality 3</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias corporis voluptate quasi? Rerum!</p>
-                      <a href="#" class="link-about-us ">About Us</a>
+                </div>
+                <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
+                    <div class="card border-0  " style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <div class="icon mb-4">
+                                <i class="fa-solid fa-user"></i></i>
+                            </div>
+                            <h5 class="card-subtitle mb-2">Quality 3</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste
+                                quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias
+                                corporis voluptate quasi? Rerum!</p>
+                            <a href="#" class="link-about-us ">About Us</a>
+                        </div>
                     </div>
-                  </div>
-               </div>
-               <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
-                <div class="card border-0  " style="width: 18rem;">
-                    <div class="card-body text-center">
-                     <div class="icon mb-4">
-                        <i class="fa-solid fa-user"></i></i>
-                     </div>
-                      <h5 class="card-subtitle mb-2">Quality 4</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias corporis voluptate quasi? Rerum!</p>
-                      <a href="#" class="link-about-us ">About Us</a>
+                </div>
+                <div class="col-lg-3 col-md-6 mt-3 col-sm-12 wrap-cards-propreties ">
+                    <div class="card border-0  " style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <div class="icon mb-4">
+                                <i class="fa-solid fa-user"></i></i>
+                            </div>
+                            <h5 class="card-subtitle mb-2">Quality 4</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum iste
+                                quidem aperiam dolor explicabo ab officia dolore ipsa rem unde fugit autem cum, nobis alias
+                                corporis voluptate quasi? Rerum!</p>
+                            <a href="#" class="link-about-us ">About Us</a>
+                        </div>
                     </div>
-                  </div>
-               </div>
+                </div>
             </div>
         </div>
     </div>
@@ -169,10 +181,11 @@
             <div class="row mt-5">
                 <div class="col text-center">
                     <h1 class="mb-3 fw-bold" style="color:rgb(0, 85, 85) ;">
-                        What people say about us 
+                        What people say about us
                     </h1>
                     <p class="">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim pariatur similique debitis vel nisi qui reprehenderit totam? Quod maiores. 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim pariatur similique debitis vel
+                        nisi qui reprehenderit totam? Quod maiores.
                     </p>
                 </div>
             </div>
@@ -191,7 +204,10 @@
                                     Wonderful experience
                                 </span>
                                 <p class="mt-2">
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis, fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi! Iusto nihil amet dolorum nemo?
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis,
+                                    fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit
+                                    voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi!
+                                    Iusto nihil amet dolorum nemo?
                                 </p>
                             </div>
                             <div class="mb-5 d-flex justify-content-center">
@@ -208,8 +224,8 @@
                                     <li class="icon-circle-wrap m-2 ">
                                         <i class="fa-brands fa-instagram"></i>
                                     </li>
-                                </ul>  
-                                
+                                </ul>
+
                             </div>
                         </div>
                     </div>
@@ -228,7 +244,10 @@
                                     Wonderful experience
                                 </span>
                                 <p class="mt-2">
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis, fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi! Iusto nihil amet dolorum nemo?
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis,
+                                    fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit
+                                    voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi!
+                                    Iusto nihil amet dolorum nemo?
                                 </p>
                             </div>
                             <div class="mb-5 d-flex justify-content-center">
@@ -246,7 +265,7 @@
                                         <i class="fa-brands fa-instagram"></i>
                                     </li>
 
-                                </ul>  
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -265,7 +284,10 @@
                                     Wonderful experience
                                 </span>
                                 <p class="mt-2">
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis, fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi! Iusto nihil amet dolorum nemo?
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quas quos facilis,
+                                    fugiat deleniti sed impedit. Aut illum, architecto corporis blanditiis voluptatum sit
+                                    voluptatem iste debitis sint nobis iusto, sapiente accusamus atque iure harum nisi!
+                                    Iusto nihil amet dolorum nemo?
                                 </p>
                             </div>
                             <div class="mb-5 d-flex justify-content-center">
