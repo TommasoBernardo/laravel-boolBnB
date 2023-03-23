aa7be30ab8e94b731e07f252cc87cd3013997c29
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-03-23 15:26:11 +0100

    add * to required input

--- resources/views/ur/apartment/shared/formCreateEdit.blade.php
+++ resources/views/ur/apartment/shared/formCreateEdit.blade.php
@@ -6,7 +6,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="title">Inserisci il titolo</label>
+            <label class="form-label" for="title">Inserisci il titolo*</label>
             <input class="form-control {{$errors->has('title') ? 'is-invalid' : '' }}" type="text" value="{{old('title',$apartment->title)}}" name="title" id="title" required maxlength="150" minlength="5">
             @if($errors->has('title'))
             <div class="alert alert-danger mt-3">
@@ -20,7 +20,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="rooms">Inserisci il numero di stanze</label>
+            <label class="form-label" for="rooms">Inserisci il numero di stanze*</label>
             <input class="form-control {{$errors->has('rooms') ? 'is-invalid' : '' }}" type="number" value="{{old('rooms',$apartment->rooms)}}" name="rooms" id="rooms" required min="1" max="255">
             @if($errors->has('rooms'))
             <div class="alert alert-danger mt-3">
@@ -34,7 +34,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="beds">Inserisci il numero di letti</label>
+            <label class="form-label" for="beds">Inserisci il numero di letti*</label>
             <input class="form-control {{$errors->has('beds') ? 'is-invalid' : '' }}" type="number" value="{{old('beds',$apartment->beds)}}" name="beds" id="beds" required min="1" max="255">
             @if($errors->has('beds'))
             <div class="alert alert-danger mt-3">
@@ -48,7 +48,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="bathrooms">Inserisci il numero di bagni</label>
+            <label class="form-label" for="bathrooms">Inserisci il numero di bagni*</label>
             <input class="form-control {{$errors->has('bathrooms') ? 'is-invalid' : '' }}" type="number" value="{{old('bathrooms',$apartment->bathrooms)}}" name="bathrooms" id="bathrooms" required min="1" max="255">
             @if($errors->has('bathrooms'))
             <div class="alert alert-danger mt-3">
@@ -62,7 +62,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="square_meters">Inserisci i metri quadri</label>
+            <label class="form-label" for="square_meters">Inserisci i metri quadri*</label>
             <input class="form-control {{$errors->has('square_meters') ? 'is-invalid' : '' }}" type="number" value="{{old('square_meters',$apartment->square_meters)}}" name="square_meters" id="square_meters" required min="30" max="4294967295">
             @if($errors->has('square_meters'))
             <div class=" alert alert-danger mt-3">
@@ -76,7 +76,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="visible">Inserisci se già disponibile: </label>
+            <label class="form-label" for="visible">Inserisci se già disponibile*: </label>
             <label class="form-label" for="visible">si</label>
             <input class="form-check-input {{$errors->has('visible') ? 'is-invalid' : '' }}" type="radio" value="1" name="visible" id="visible" @if(isset($apartment->visible) || (old('visible') != null)) @if(old('visible',$apartment->visible) == 1) checked @endif @endif required>
             <label class="form-label" for="visible">no</label>
@@ -94,7 +94,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="address">Inserisci l'indirizzo: </label>
+            <label class="form-label" for="address">Inserisci l'indirizzo*: </label>
             <div id="searchBox">
 
             </div>
@@ -123,7 +123,7 @@
 
         <div class="mb-3">
 
-            <label class="form-label" for="cover_image">Inserisci un'immagine di cover: </label>
+            <label class="form-label" for="cover_image">Inserisci un'immagine di cover*: </label>
             <input class="form-control {{$errors->has('cover_image') ? 'is-invalid' : '' }}" type="file" value="{{old('cover_image',$apartment->cover_image)}}" name="cover_image" id="cover_image">
             @if($errors->has('cover_image'))
             <div class="alert alert-danger mt-3">
@@ -136,6 +136,7 @@
         </div>
 
         <div class="mb-3">
+            <p for="">Seleziona i servizi del tuo appartamento*:</p>
             @foreach($services as $service)
             <input type="checkbox" class="form-check-input {{$errors->has('services') ? 'is-invalid' : '' }} " name="services[]" value="{{$service->id}}" @if($errors->any())
             @checked(in_array($service->id,old('services',[])))
