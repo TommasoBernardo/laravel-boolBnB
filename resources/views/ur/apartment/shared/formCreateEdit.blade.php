5a51f6c192f01d3e2cc3233eb86944a69c64e88a
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-03-24 12:16:37 +0100

    edit mex error for search bar

--- resources/views/ur/apartment/shared/formCreateEdit.blade.php
+++ resources/views/ur/apartment/shared/formCreateEdit.blade.php
@@ -98,7 +98,7 @@
             <div id="searchBox">
 
             </div>
-            <p id="mexErrore" class="text-center text-danger fs-1 m-0 d-none">Selezionare dal menu a tendina</p>
+            <p id="mexErrore" class="text-center text-danger fs-1 m-0 d-none">Selezionare una via dal menu a tendina</p>
             @if($errors->has('address'))
             <div class="alert alert-danger mt-3">
                 @foreach ($errors->get('address') as $error)
