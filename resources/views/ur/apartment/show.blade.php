42f332a28b18087980127581ba82f02aee664d33
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-27 17:26:07 +0200

    implemented slider and fix layout

--- resources/views/ur/apartment/show.blade.php
+++ resources/views/ur/apartment/show.blade.php
@@ -2,79 +2,102 @@
 @section('scss')
     @vite(['resources/js/show.js'])
 @endsection
+
 @section('content')
     <div class="d-none">
         <input type="text" id="latitude" value="{{ $apartment->latitude }}">
         <input type="text" id="longitude" value="{{ $apartment->longitude }}">
     </div>
-    <div class="container img-container p-5 my-4 ">
-        <h1 class="m-4 text-md-center">{{ $apartment->title }}</h1>
-        <div class="row p-5">
-            <div class="col-lg-6 col-md-12">
-                <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
-                    alt="{{ $apartment->title }}">
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    @foreach ($apartment->images as $image)
-                        <div class="col-sm-6">
-                            <img src="{{ asset('storage/' . $image->path) }}" class="card-img-top img-fluid"
-                                alt="{{ $apartment->title }}">
-                        </div>
-                    @endforeach
+    {{-- Return Arrow --}}
+    <div class="container-fluid return-arrow sticky-lg-top sticky-md-top sticky-sm-top mt-3 ">
+        <div class="row">
+            <div class="col-2">
+                <div class="wrapper return-arrow position-absolute">
+                    <a href="{{ route('apartment.index') }}" class="btn fw-bold my-4 ms-2"
+                        style="background-color: #00555595; color:white; padding: .6rem 1.2rem; font-size: 1.1rem ">
+                        <i class="fa-solid fa-arrow-left fs-3"></i>
+                    </a>
                 </div>
             </div>
         </div>
-        <div class=" container info p-3">
-            <div class="row px-2">
-                <h3 class="my-2">Info appartamento</h3>
-                <div class="col-lg-6  col-md-6 col-sm-12">
-                    <p>Numero di stanze: {{ $apartment->rooms }}</p>
+    </div>
+    {{-- Slider --}}
+    <div class="container img-container">
+        <h1 class="m-4 text-md-center text-sm-center">{{ $apartment->title }}</h1>
+        <div style="--swiper-navigation-color: #ffffff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                        alt="{{ $apartment->title }}">
+                </div>
+                @foreach ($apartment->images as $image)
+                    <div class="swiper-slide">
+                        <img src="{{ asset('storage/' . $image->path) }}"class="card-img-top img-fluid"
+                            alt="{{ $apartment->title }}">
+                    </div>
+                @endforeach
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </div>
+        </div>
+        <div class="swiper-thumb mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top img-fluid"
+                        alt="{{ $apartment->title }}">
+                </div>
+                @foreach ($apartment->images as $image)
+                    <div class="swiper-slide">
+                        <img src="{{ asset('storage/' . $image->path) }}"class="card-img-top img-fluid"
+                            alt="{{ $apartment->title }}">
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="container info-wrapper d-flex mt-5">
+        <div class=" container info">
+            <div class="row  mt-md-0">
+                <h3 class="my-2">Apartment information</h3>
+                <div class="col-lg-6 col-md-12 col-sm-12 fs-5">
+                    <p class="m-0">Numero di stanze: {{ $apartment->rooms }}</p>
                     <p>Numero di letti: {{ $apartment->beds }}</p>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                    <p>Numero di bagni: {{ $apartment->bathrooms }}</p>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 fs-5">
+                    <p class="m-0">Numero di bagni: {{ $apartment->bathrooms }}</p>
                     <p>Dimensioni: {{ $apartment->square_meters }} mq</p>
                 </div>
             </div>
         </div>
-        <div class="services p-3 my-3">
-            <div class="container">
-                <div class="row px-2">
-                    <h3 class="my-2">Servizi offerti</h3>
-                    @foreach ($apartment->services as $service)
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            <span class="icon m-1 fs-4">
-                                {!! $service->icon !!} &nbsp {{ $service->type }}
-                            </span>
-                        </div>
-                    @endforeach
-                </div>
+        {{-- Servizi --}}
+        <div class="container services">
+            <div class="row ">
+                <h3 class="my-2">Offered services</h3>
+                @foreach ($apartment->services as $service)
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 fs-5">
+                        {!! $service->icon !!}
+                        <p class="services d-inline">
+                            &nbsp {{ $service->type }}
+                        </p>
+                    </div>
+                @endforeach
             </div>
         </div>
-        <div class="container location">
-            <h3 class="d-inline-block my-3">Si trova in </h3>
-            <div class="row">
-                <div class="col-lg-12">
-                    <span class="fs-5"> {{ $apartment->address }}</span>
-                    <div class="map my-2" id="map">
-                    </div>
+    </div>
+
+    <div class="container location mt-3 mb-5">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="d-inline-block my-2 me-2 ">Is located in</h3>
+                <span class="fs-3 fw-bold"> {{ $apartment->address }}</span>
+                <div class="map my-2" id="map">
                 </div>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row mt-5 mb-5">
-            <div class="col d-flex justify-content-center">
-                <div class="wrapper-return-index">
-                    <a href="{{ route('apartment.index') }}" class="btn fw-bold"
-                        style="background-color: #005555; color:white; padding: .8rem 2rem; font-size: 1.1rem ">Return to
-                        your apartment</a>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
 @section('script')
-    @vite(['resources/js/mapShow.js', 'resources/js/popupDelete.js'])
+    @vite(['resources/js/mapShow.js', 'resources/js/popupDelete.js', 'resources/js/show-slider.js'])
 @endsection
