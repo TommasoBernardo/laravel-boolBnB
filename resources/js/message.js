88b9683fbf4346a4da47e52ab37015f6d87b0f30
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-28 14:04:20 +0200

    add hover  box to message table column

---
+++ resources/js/message.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import '~resources/scss/app.scss';
+import '~resources/scss/message.scss';
+import * as bootstrap from 'bootstrap';
+import.meta.glob([
+    '../img/**'
+])
--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,5 +1,9 @@
+@section('scss')
+    @vite(['resources/js/message.js'])
+@endsection
+
 <div class="container">
-    <div class="row"  style="margin-top: 3rem; margin-bottom: 4rem " >
+    <div class="row" style="margin-top: 3rem; margin-bottom: 4rem ">
         <div class="col-12 text-center">
             <h1> Message from User </h1>
         </div>
@@ -7,45 +11,99 @@
     @php $count = 0; @endphp
     @php $count++; @endphp
 </div>
-<div class="container-fluid">
+{{-- <div class="container-fluid">
     <div class="row m-5">
-        <table class="table">
-            <thead>
-                <tr class="text-center">
-                    <th >Apartment</th>
-                    <th >User email</th>
-                    <th >Name</th>
-                    <th >Phone Number </th>
-                    <th >Message</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($apartments as $apartment)
-                    @foreach ($apartment->leads as $lead)
-                        @if ($lead->show != 0)
-                            <tr class="">
-                                <th> {{ $apartment->title }} </th>
-                                <td>{{ $lead->email }} </td>
-                                <td>{{ $lead->name }} </td>
-                                <td>{{ $lead->phone_number }} </td>
-                                <td >{{ $lead->message }}</td>
-                                <td > 
-                                    <form action="{{route('dashboard.messageUpdate',$lead->id )}}" method="POST" >
-                                    @csrf
-                                    @method('PUT')
-                                    <button type="submit" class="btn btn-primary">Presa visione  </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endif
+        <div class="col-lg-12 ">
+            <table class="table ">
+                <thead>
+                    <tr class=" text-center">
+                        <th>Apartment</th>
+                        <th>User email</th>
+                        <th>Name</th>
+                        <th>Phone Number </th>
+                        <th>Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($apartments as $apartment)
+                        @foreach ($apartment->leads as $lead)
+                            @if ($lead->show != 0)
+                                <tr>
+                                    <td> {{ $apartment->title }} </td>
+                                    <td>{{ $lead->email }} </td>
+                                    <td>{{ $lead->name }} </td>
+                                    <td>{{ $lead->phone_number }} </td> 
+                                    <td>{{ $lead->message }} </td>
+                                    <td>
+                                        <form action="{{ route('dashboard.messageUpdate', $lead->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="btn btn-primary">Presa visione </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endif
+                        @endforeach
                     @endforeach
-                @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
     @if ($count == 0)
         <p class="alert alert-danger ">Nessun messaggio da visualizzare.</p> -
     @endif
 
-</div>
+</div> --}}
 
+<div class="container-fluid">
+    <div class="row">
+        <section class="content-area">
+            <div class="table-area">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Apartment</th>
+                            <th>User email</th>
+                            <th>Name</th>
+                            <th>Message</th>
+                            <th>Phone Number </th>
+                            <th>Presa visione </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($apartments as $apartment)
+                            @foreach ($apartment->leads as $lead)
+                                @if ($lead->show != 0)
+                                    <tr>
+                                        <td> {{ $apartment->title }} </td>
+                                        <td>{{ $lead->email }} </td>
+                                        <td>{{ $lead->name }} </td>
+                                        <td class='has-details'>
+                                          Hover to see message
+                                          <span class="details">{{ $lead->message }}</span>
+                                        </td>
+                                        <td>{{ $lead->phone_number }} </td>
+                                        {{-- <td>{{ $lead->message }} </td> --}}
+                                        <td>
+                                            <form action="{{ route('dashboard.messageUpdate', $lead->id) }}"
+                                                method="POST">
+                                                @csrf
+                                                @method('PUT')
+                                                <button type="submit" class="btn btn-success"><i class="fa-solid fa-eye"></i> </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                 
+                                        
+                                @endif
+                            @endforeach
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </div>
+    @if ($count == 0)
+    <p class="alert alert-danger ">Nessun messaggio da visualizzare.</p> -
+    @endif
+</div>
