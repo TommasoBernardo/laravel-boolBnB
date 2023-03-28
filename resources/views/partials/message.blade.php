3998abda125e9fe4c53bd3cf2a08dc38c161cc46
MatteoCarbone17 <matteocarbone17@live.com>
2023-03-28 11:54:29 +0200

    imp layout message.blade

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,44 +1,45 @@
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1>Messaggi da parte di utenti:</h1>
+    <div class="row"  style="margin-top: 3rem; margin-bottom: 4rem " >
+        <div class="col-12 text-center">
+            <h1> Message from User </h1>
         </div>
     </div>
-
     @php $count = 0; @endphp
 
-
-    @foreach ($apartments as $apartment)
-    @foreach($apartment->leads as $lead)
-    @if($lead->show != 0)
     @php $count++; @endphp
-    <h1>{{ $apartment->title }}</h1>
-    <div class="card text-center">
-        <div class="card-header">
-            {{ $lead->name }}  {{ $lead->phone_number }}
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">{{ $lead->email }} </h5>
-            <p class="card-text">{{ $lead->message }}</p>
-            <form action="{{route('dashboard.messageUpdate',$lead->id)}}" class="d-inline-block ms-3" method="POST">
-
-                @csrf
-                @method('PUT')
-
-                <button type="submit" class="btn btn-primary">Presa visione</button>
-            </form>
-        </div>
-        <div class="card-footer text-muted">
-            {{$lead->created_at}}
-        </div>
+</div>
+<div class="container-fluid">
+    <div class="row m-5">
+        <table class="table">
+            <thead>
+                <tr class="text-center">
+                    <th >Apartment</th>
+                    <th >User email</th>
+                    <th >Name</th>
+                    <th >Phone Number </th>
+                    <th >Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($apartments as $apartment)
+                    @foreach ($apartment->leads as $lead)
+                        @if ($lead->show != 0)
+                            <tr  >
+                                <th> {{ $apartment->title }} </th>
+                                <td>{{ $lead->email }} </td>
+                                <td>{{ $lead->name }} </td>
+                                <td>{{ $lead->phone_number }} </td>
+                                <td >{{ $lead->message }}</td>
+                            </tr>
+                        @endif
+                    @endforeach
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    @endif
-    @endforeach
-
-    @endforeach
-
-    @if($count == 0)
-    <p class="text-center fs-1 text-danger">Nessun messaggio da visualizzare.</p>
+    @if ($count == 0)
+        <p class="alert alert-danger ">Nessun messaggio da visualizzare.</p> -
     @endif
 
 </div>
+
