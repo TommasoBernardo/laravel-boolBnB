a2c994085370a55ff6c646f2126c23dcb6666794
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-03-28 14:23:35 +0200

    modify popup message

--- resources/js/popupDelete.js
+++ resources/js/popupDelete.js
@@ -3,14 +3,14 @@ var popup = document.getElementById('popup_message');
 if (popup) {
     // show a message in a toast
     Swal.fire({
-        toast: true,
-        animation: false,
+        // toast: true,
+        // animation: false,
         icon: popup.dataset.type,
         title: popup.dataset.message,
         type: popup.dataset.type,
-        position: 'top-right',
-        timer: 3000,
-        showConfirmButton: false,
+        // position: 'top-right',
+        // timer: 3000,
+        showConfirmButton: true,
     });
 }
 
